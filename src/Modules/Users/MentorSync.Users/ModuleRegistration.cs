85a8e094d58e090ae2e5baef31b717624ae0b3ae
Andriy Borkovich <borkovich25andri@gmail.com>
2025-01-27 23:08:50 +0200

    migrated to .NET 9

---
+++ src/Modules/Users/MentorSync.Users/ModuleRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MentorSync.Users;
+
+public static class ModuleRegistration
+{
+    public static IServiceCollection AddUsersModule(this IServiceCollection services)
+    {
+        return services;
+    }
+}
