7be90cbd325de962647e7f418c281290caf768da
Andrii Borkovych <92755910+AndriyBorkovich@users.noreply.github.com>
2025-02-15 23:38:15 +0200

    feat: migrated from container to Azure PostgreSQL DB service (#20)
    Also reconfigured deploy workflow to run only after the build and test success

--- aspire/MentorSync.AppHost/Program.cs
+++ aspire/MentorSync.AppHost/Program.cs
@@ -1,11 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // postgres database
-var postgres = builder.AddPostgres("postgres-db")
-                        .WithPgAdmin(containerBuilder =>
-                            containerBuilder.WithLifetime(ContainerLifetime.Persistent))
-                        .WithDataVolume(name: "postgres-data", isReadOnly: false)
-                        .WithLifetime(ContainerLifetime.Persistent);
+var postgres = builder.AddAzurePostgresFlexibleServer("postgres-db");
+                       // for local development
+                    //    .RunAsContainer(b => 
+                    //     {
+                    //         b.WithPgAdmin(cb => cb.WithLifetime(ContainerLifetime.Persistent));
+                    //         b.WithDataVolume(name: "postgres-data", isReadOnly: false);
+                    //         b.WithLifetime(ContainerLifetime.Persistent);
+                    //     });
 var postgresDb = postgres.AddDatabase("MentorSyncDb");
 
 // API project
