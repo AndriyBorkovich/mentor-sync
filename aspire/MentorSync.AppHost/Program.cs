0327799582832c6a1cda4c24c4d3643a32aaae7c
Andriy Borkovich <borkovich25andri@gmail.com>
2025-02-16 21:58:41 +0200

    enabled local development for PostgreSQL

--- aspire/MentorSync.AppHost/Program.cs
+++ aspire/MentorSync.AppHost/Program.cs
@@ -3,15 +3,20 @@ var builder = DistributedApplication.CreateBuilder(args);
 // postgres database
 var username = builder.AddParameter("username", secret: true);
 var password = builder.AddParameter("password", secret: true);
+
 var postgres = builder.AddAzurePostgresFlexibleServer("postgres-db")
-                    .WithPasswordAuthentication(username, password);
-                       // for local development
-                    //    .RunAsContainer(b => 
-                    //     {
-                    //         b.WithPgAdmin(cb => cb.WithLifetime(ContainerLifetime.Persistent));
-                    //         b.WithDataVolume(name: "postgres-data", isReadOnly: false);
-                    //         b.WithLifetime(ContainerLifetime.Persistent);
-                    //     });
+                      .WithPasswordAuthentication(username, password);
+
+if(!builder.ExecutionContext.IsPublishMode)
+{
+    postgres = postgres.RunAsContainer(b => 
+    {
+        b.WithPgAdmin(cb => cb.WithLifetime(ContainerLifetime.Persistent));
+        b.WithDataVolume(name: "postgres-data", isReadOnly: false);
+        b.WithLifetime(ContainerLifetime.Persistent);
+    });
+}
+
 var postgresDb = postgres.AddDatabase("MentorSyncDb");
 
 // API project
